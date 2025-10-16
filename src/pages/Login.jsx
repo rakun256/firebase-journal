32f55301c5655c72d9c3db7b7565fb04bb64c5ec
Emre Uslu <emre.uslu@d-teknoloji.com.tr>
2025-10-16 16:53:38 +0300

    feat: add Google login functionality to the Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { auth } from "../lib/firebase";
 import { Link, useNavigate, Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
@@ -12,6 +16,7 @@ export default function Login() {
   const [error, setError] = useState("");
   const { showToast } = useToast();
   const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
 
   if (user) return <Navigate to="/" replace />;
 
@@ -27,6 +32,14 @@ export default function Login() {
       setError(e.message);
     }
   };
+
+  async function handleGoogleLogin() {
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   return (
     <>
@@ -74,6 +87,38 @@ export default function Login() {
                 Log In
               </button>
             </form>
+            <button
+              onClick={handleGoogleLogin}
+              className="flex cursor-pointer mt-4 items-center justify-between w-auto max-w-[400px] min-w-min h-10 px-3 bg-[#f2f2f2] rounded-[20px] font-roboto text-[14px] text-[#1f1f1f] font-medium tracking-[0.25px] transition-all duration-200 hover:shadow-[0_1px_2px_rgba(60,64,67,0.3),0_1px_3px_1px_rgba(60,64,67,0.15)] active:before:opacity-10 focus:before:opacity-10 relative overflow-hidden select-none"
+            >
+              <div className="absolute inset-0 opacity-0 transition-opacity duration-200 pointer-events-none before:content-[''] before:absolute before:inset-0 before:bg-[#001d35] before:opacity-0"></div>
+              <div className="flex items-center h-full w-full relative">
+                <div className="w-5 h-5 mr-3 min-w-5">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                    <path
+                      fill="#EA4335"
+                      d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
+                    />
+                    <path
+                      fill="#4285F4"
+                      d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
+                    />
+                    <path
+                      fill="#FBBC05"
+                      d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
+                    />
+                    <path
+                      fill="#34A853"
+                      d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
+                    />
+                    <path fill="none" d="M0 0h48v48H0z" />
+                  </svg>
+                </div>
+                <span className="flex-grow overflow-hidden text-ellipsis">
+                  Continue with Google
+                </span>
+              </div>
+            </button>
           </div>
         </section>
       </div>
@@ -117,6 +162,39 @@ export default function Login() {
                 Log In
               </button>
             </form>
+            <button
+              onClick={handleGoogleLogin}
+              className="flex mt-4 cursor-pointer items-center justify-between w-auto max-w-[400px] min-w-min h-10 px-3 bg-[#f2f2f2] rounded-[20px] font-roboto text-[14px] text-[#1f1f1f] font-medium tracking-[0.25px] transition-all duration-200 hover:shadow-[0_1px_2px_rgba(60,64,67,0.3),0_1px_3px_1px_rgba(60,64,67,0.15)] active:before:opacity-10 focus:before:opacity-10 relative overflow-hidden select-none"
+            >
+              <div className="absolute inset-0 opacity-0 transition-opacity duration-200 pointer-events-none before:content-[''] before:absolute before:inset-0 before:bg-[#001d35] before:opacity-0"></div>
+              <div className="flex items-center h-full w-full relative">
+                <div className="w-5 h-5 mr-3 min-w-5">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                    <path
+                      fill="#EA4335"
+                      d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
+                    />
+                    <path
+                      fill="#4285F4"
+                      d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
+                    />
+                    <path
+                      fill="#FBBC05"
+                      d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
+                    />
+                    <path
+                      fill="#34A853"
+                      d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
+                    />
+                    <path fill="none" d="M0 0h48v48H0z" />
+                  </svg>
+                </div>
+                <span className="flex-grow overflow-hidden text-ellipsis">
+                  Continue with Google
+                </span>
+              </div>
+            </button>
+
             <h2 className="mt-16">Don't have an account?</h2>
             <Link
               to="/signup"
@@ -128,5 +206,5 @@ export default function Login() {
         </section>
       </div>
     </>
-  )
+  );
 }
